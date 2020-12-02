5181f95690cb98345179fab2700114285ae4dce7
Adam Nemecek <adamnemecek@gmail.com>
2020-12-01 17:42:56 -0800

    added files

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,321 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+mod analyser_node;
+pub use analyser_node::*;
+
+mod analyser_options;
+pub use analyser_options::*;
+
+mod async_audio_decoder;
+pub use async_audio_decoder::*;
+
+mod audio_basic_inspector_node;
+pub use audio_basic_inspector_node::*;
+
+mod audio_basic_processor_handler;
+pub use audio_basic_processor_handler::*;
+
+mod audio_basic_processor_handler_test;
+pub use audio_basic_processor_handler_test::*;
+
+mod audio_buffer;
+pub use audio_buffer::*;
+
+mod audio_buffer_options;
+pub use audio_buffer_options::*;
+
+mod audio_buffer_source_node;
+pub use audio_buffer_source_node::*;
+
+mod audio_buffer_source_options;
+pub use audio_buffer_source_options::*;
+
+mod audio_context;
+pub use audio_context::*;
+
+mod audio_context_autoplay_test;
+pub use audio_context_autoplay_test::*;
+
+mod audio_context_options;
+pub use audio_context_options::*;
+
+mod audio_context_test;
+pub use audio_context_test::*;
+
+mod audio_destination_node;
+pub use audio_destination_node::*;
+
+mod audio_graph_tracer;
+pub use audio_graph_tracer::*;
+
+mod audio_listener;
+pub use audio_listener::*;
+
+mod audio_node;
+pub use audio_node::*;
+
+mod audio_node_input;
+pub use audio_node_input::*;
+
+mod audio_node_input_test;
+pub use audio_node_input_test::*;
+
+mod audio_node_options;
+pub use audio_node_options::*;
+
+mod audio_node_output;
+pub use audio_node_output::*;
+
+mod audio_node_wiring;
+pub use audio_node_wiring::*;
+
+mod audio_param;
+pub use audio_param::*;
+
+mod audio_param_descriptor;
+pub use audio_param_descriptor::*;
+
+mod audio_param_map;
+pub use audio_param_map::*;
+
+mod audio_param_timeline;
+pub use audio_param_timeline::*;
+
+mod audio_processing_event;
+pub use audio_processing_event::*;
+
+mod audio_processing_event_init;
+pub use audio_processing_event_init::*;
+
+mod audio_scheduled_source_node;
+pub use audio_scheduled_source_node::*;
+
+mod audio_summing_junction;
+pub use audio_summing_junction::*;
+
+mod audio_timestamp;
+pub use audio_timestamp::*;
+
+mod audio_worklet;
+pub use audio_worklet::*;
+
+mod audio_worklet_global_scope;
+pub use audio_worklet_global_scope::*;
+
+mod audio_worklet_global_scope_test;
+pub use audio_worklet_global_scope_test::*;
+
+mod audio_worklet_messaging_proxy;
+pub use audio_worklet_messaging_proxy::*;
+
+mod audio_worklet_node;
+pub use audio_worklet_node::*;
+
+mod audio_worklet_node_options;
+pub use audio_worklet_node_options::*;
+
+mod audio_worklet_object_proxy;
+pub use audio_worklet_object_proxy::*;
+
+mod audio_worklet_processor;
+pub use audio_worklet_processor::*;
+
+mod audio_worklet_processor_definition;
+pub use audio_worklet_processor_definition::*;
+
+mod audio_worklet_processor_error_state;
+pub use audio_worklet_processor_error_state::*;
+
+mod audio_worklet_thread_test;
+pub use audio_worklet_thread_test::*;
+
+mod base_audio_context;
+pub use base_audio_context::*;
+
+mod biquad_dsp_kernel;
+pub use biquad_dsp_kernel::*;
+
+mod biquad_filter_node;
+pub use biquad_filter_node::*;
+
+mod biquad_filter_options;
+pub use biquad_filter_options::*;
+
+mod biquad_processor;
+pub use biquad_processor::*;
+
+mod BUILD;
+pub use BUILD::*;
+
+mod channel_merger_node;
+pub use channel_merger_node::*;
+
+mod channel_merger_options;
+pub use channel_merger_options::*;
+
+mod channel_splitter_node;
+pub use channel_splitter_node::*;
+
+mod channel_splitter_options;
+pub use channel_splitter_options::*;
+
+mod constant_source_node;
+pub use constant_source_node::*;
+
+mod constant_source_options;
+pub use constant_source_options::*;
+
+mod convolver_node;
+pub use convolver_node::*;
+
+mod convolver_node_test;
+pub use convolver_node_test::*;
+
+mod convolver_options;
+pub use convolver_options::*;
+
+mod cross_thread_audio_worklet_processor_info;
+pub use cross_thread_audio_worklet_processor_info::*;
+
+mod deferred_task_handler;
+pub use deferred_task_handler::*;
+
+mod delay_dsp_kernel;
+pub use delay_dsp_kernel::*;
+
+mod delay_node;
+pub use delay_node::*;
+
+mod delay_options;
+pub use delay_options::*;
+
+mod delay_processor;
+pub use delay_processor::*;
+
+mod dynamics_compressor_node;
+pub use dynamics_compressor_node::*;
+
+mod dynamics_compressor_node_test;
+pub use dynamics_compressor_node_test::*;
+
+mod dynamics_compressor_options;
+pub use dynamics_compressor_options::*;
+
+mod gain_node;
+pub use gain_node::*;
+
+mod gain_options;
+pub use gain_options::*;
+
+mod idls;
+pub use idls::*;
+
+mod iir_dsp_kernel;
+pub use iir_dsp_kernel::*;
+
+mod iir_filter_node;
+pub use iir_filter_node::*;
+
+mod iir_filter_options;
+pub use iir_filter_options::*;
+
+mod iir_processor;
+pub use iir_processor::*;
+
+mod inspector_helper_mixin;
+pub use inspector_helper_mixin::*;
+
+mod inspector_web_audio_agent;
+pub use inspector_web_audio_agent::*;
+
+mod media_element_audio_source_node;
+pub use media_element_audio_source_node::*;
+
+mod media_element_audio_source_options;
+pub use media_element_audio_source_options::*;
+
+mod media_stream_audio_destination_node;
+pub use media_stream_audio_destination_node::*;
+
+mod media_stream_audio_source_node;
+pub use media_stream_audio_source_node::*;
+
+mod media_stream_audio_source_options;
+pub use media_stream_audio_source_options::*;
+
+mod offline_audio_completion_event;
+pub use offline_audio_completion_event::*;
+
+mod offline_audio_completion_event_init;
+pub use offline_audio_completion_event_init::*;
+
+mod offline_audio_context;
+pub use offline_audio_context::*;
+
+mod offline_audio_context_options;
+pub use offline_audio_context_options::*;
+
+mod offline_audio_destination_node;
+pub use offline_audio_destination_node::*;
+
+mod offline_audio_worklet_thread;
+pub use offline_audio_worklet_thread::*;
+
+mod oscillator_node;
+pub use oscillator_node::*;
+
+mod oscillator_options;
+pub use oscillator_options::*;
+
+mod panner_node;
+pub use panner_node::*;
+
+mod panner_options;
+pub use panner_options::*;
+
+mod periodic_wave;
+pub use periodic_wave::*;
+
+mod periodic_wave_constraints;
+pub use periodic_wave_constraints::*;
+
+mod periodic_wave_options;
+pub use periodic_wave_options::*;
+
+mod realtime_analyser;
+pub use realtime_analyser::*;
+
+mod realtime_audio_destination_node;
+pub use realtime_audio_destination_node::*;
+
+mod realtime_audio_worklet_thread;
+pub use realtime_audio_worklet_thread::*;
+
+mod script_processor_node;
+pub use script_processor_node::*;
+
+mod script_processor_node_test;
+pub use script_processor_node_test::*;
+
+mod semi_realtime_audio_worklet_thread;
+pub use semi_realtime_audio_worklet_thread::*;
+
+mod stereo_panner_node;
+pub use stereo_panner_node::*;
+
+mod stereo_panner_node_test;
+pub use stereo_panner_node_test::*;
+
+mod stereo_panner_options;
+pub use stereo_panner_options::*;
+
+mod wave_shaper_dsp_kernel;
+pub use wave_shaper_dsp_kernel::*;
+
+mod wave_shaper_node;
+pub use wave_shaper_node::*;
+
+mod wave_shaper_options;
+pub use wave_shaper_options::*;
+
+mod wave_shaper_processor;
+pub use wave_shaper_processor::*;
+
